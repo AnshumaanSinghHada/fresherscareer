db747de655e78ed80b23f43614d139cf10b3667a
Anshumaan Singh Hada <69346510+AnshumaanSinghHada@users.noreply.github.com>
2024-03-15 01:50:40 +0530

    Update JobList.ts

--- JobList.ts
+++ JobList.ts
@@ -1,28 +1,28 @@
 export const JobList: Job[] = [
   {
     id: 1,
-    heading: "Accenture Job A",
-    link: "https://www.google.com",
+    heading: "Technical Process Executive | Infosys | Pune",
+    link: "https://career.infosys.com/jobdesc?source=44003&jobReferenceCode=PROGEN-EXTERNAL-175528",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 2,
     heading:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quoscommodi quibusdam maiores eveniet doloremque explicabo, quasinobis sint perspiciatis id quis aspernatur culpa hic enimveritatis quisquam animi exercitationem deleniti.",
-    link: "https://www.google.com",
+      "Software Engineer | Gap Inc. | Hyderabad",
+    link: "https://www.gapinc.com/en-in/jobs/w96/16/software-engineer-graduate-engineer-trainee",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 3,
-    heading: "Deloitte Job B",
-    link: "https://www.google.com",
+    heading: "Software Developer | Ericsson | Bangalore",
+    link: "https://jobs.ericsson.com/careers?query=Software%20Development&location=India&pid=563121758389063&domain=ericsson.com&sort_by=relevance&utm_source=ericsson.com&utm_campaign=search_widget&triggerGoButton=false&triggerGoButton=true",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 4,
     heading:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quoscommodi quibusdam maiores eveniet doloremque explicabo, quasinobis sint perspiciatis id quis aspernatur culpa hic enimveritatis quisquam animi exercitationem deleniti.",
-    link: "https://www.google.com",
+      "Data Analyst | Mercedes Benz | Bangalore",
+    link: "https://jobs.mercedes-benz.com/en/data-analyst-64089-MER0002HZW",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
@@ -34,14 +34,14 @@ export const JobList: Job[] = [
   {
     id: 6,
     heading:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quoscommodi quibusdam maiores eveniet doloremque explicabo, quasinobis sint perspiciatis id quis aspernatur culpa hic enimveritatis quisquam animi exercitationem deleniti.",
-    link: "https://www.google.com",
+      "Associate Analyst | Deloitte | Hyderabad",
+    link: "https://usijobs.deloitte.com/careersUSI/JobDetail/USI-EH24-EA-Talent-Services-Learning-Operations-Associate-Analyst-HYD/158978",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 7,
-    heading: "Deloitte Job D",
-    link: "https://www.google.com",
+    heading: "System Engineer | Meta | Bengaluru",
+    link: "https://unstop.com/jobs/hardware-systems-engineer-silicon-meta-925282?utm_source=JobssForYou&utm_medium=Affiliates&utm_campaign=META08032024&ref=JobssForYou",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
@@ -53,28 +53,46 @@ export const JobList: Job[] = [
   },
   {
     id: 9,
-    heading: "Deloitte Job E",
-    link: "https://www.google.com",
+    heading: "System Engineer | Honeywell | Pune",
+    link: "https://careers.honeywell.com/us/en/job/HONEUSHRD224165EXTERNALENUS/Systems-Engr-I",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 10,
     heading:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quoscommodi quibusdam maiores eveniet doloremque explicabo, quasinobis sint perspiciatis id quis aspernatur culpa hic enimveritatis quisquam animi exercitationem deleniti.",
-    link: "https://www.google.com",
+      "Data Engineer | Intel | Pan India",
+    link: "https://jobs.intel.com/en/job/-/-/599/62306129232",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 11,
-    heading: "Deloitte Job F",
-    link: "https://www.google.com",
+    heading: "Process Analyst | HCL Tech | Nagpur",
+    link: "https://www.hcltech.com/jobs/analyst-0",
     image_url: "/static/img/Deloitte_Logo.png",
   },
   {
     id: 12,
     heading:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quoscommodi quibusdam maiores eveniet doloremque explicabo, quasinobis sint perspiciatis id quis aspernatur culpa hic enimveritatis quisquam animi exercitationem deleniti.",
-    link: "https://www.google.com",
+      "Software Engineer | Accenture | Bengaluru",
+    link: "https://www.accenture.com/in-en/careers/jobdetails?id=ATCI-4154481-S1608969_en",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+   {
+    id: 13,
+    heading: "Software Engineer | Principal | Pune",
+    link: "https://careers.principal.com/in/jobs/42115/job",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+   {
+    id: 14,
+    heading: "Software Engineer | Walmart | Bangalore",
+    link: "https://walmart.wd5.myworkdayjobs.com/en-US/WalmartExternal/details/SOFTWARE-ENGINEER-III_R-1773548?timeType=b181d8271e36017533d4ca68eee44f00&locationCountry=c4f78be1a8f14da0ab49ce1162348a5e",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+   {
+    id: 15,
+    heading: "Developer | Wipro | Bengaluru",
+    link: "https://careers.wipro.com/careers-home/jobs/3058201",
     image_url: "/static/img/Deloitte_Logo.png",
   },
 ];
