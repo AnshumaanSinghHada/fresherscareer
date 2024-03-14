6ead3db599958e734e8faf80f76de0eedd76c073
Anshumaan Singh Hada <69346510+AnshumaanSinghHada@users.noreply.github.com>
2024-03-15 02:20:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4830579227567887"
-     crossorigin="anonymous"></script>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
