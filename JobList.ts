8c19ed984ddc2752bd9498483daba247a4d30529
Anshumaan Singh Hada <69346510+AnshumaanSinghHada@users.noreply.github.com>
2024-03-18 21:27:25 +0530

    Update JobList.ts

--- JobList.ts
+++ JobList.ts
@@ -1,6 +1,24 @@
 export const JobList: Job[] = [
   {
     id: 1,
+    heading: "Associate | Cognizant | Kolkata",
+    link: "https://careers.cognizant.com/in/en/job/COGNGLOBAL00055692593/Associate-Projects",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+  {
+    id: 1,
+    heading: "Data Analyst | Johnson | Pune",
+    link: "https://jnjc.taleo.net/careersection/2/jobdetail.ftl?job=2406174230W&lang=en&",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+  {
+    id: 1,
+    heading: "QA Analyst | Infor | Hyderabad",
+    link: "https://careers.infor.com/en_US/careers/JobDetail/Quality-Assurance-Analyst-Associate/13428",
+    image_url: "/static/img/Deloitte_Logo.png",
+  },
+  {
+    id: 1,
     heading: "Technical Process Executive | Infosys | Pune",
     link: "https://career.infosys.com/jobdesc?source=44003&jobReferenceCode=PROGEN-EXTERNAL-175528",
     image_url: "/static/img/Deloitte_Logo.png",
