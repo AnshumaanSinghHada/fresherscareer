d6769da86ddcf19d8741fa1489a7ad9f011b96fb
Anshumaan Singh Hada <69346510+AnshumaanSinghHada@users.noreply.github.com>
2024-03-18 21:30:55 +0530

    Update JobList.ts

--- JobList.ts
+++ JobList.ts
@@ -3,19 +3,19 @@ export const JobList: Job[] = [
     id: 1,
     heading: "Associate | Cognizant | Kolkata",
     link: "https://careers.cognizant.com/in/en/job/COGNGLOBAL00055692593/Associate-Projects",
-    image_url: "/static/img/Deloitte_Logo.png",
+    image_url: "/static/img/Cognizant.png",
   },
   {
     id: 1,
     heading: "Data Analyst | Johnson | Pune",
     link: "https://jnjc.taleo.net/careersection/2/jobdetail.ftl?job=2406174230W&lang=en&",
-    image_url: "/static/img/Deloitte_Logo.png",
+    image_url: "/static/img/jnj.png",
   },
   {
     id: 1,
     heading: "QA Analyst | Infor | Hyderabad",
     link: "https://careers.infor.com/en_US/careers/JobDetail/Quality-Assurance-Analyst-Associate/13428",
-    image_url: "/static/img/Deloitte_Logo.png",
+    image_url: "/static/img/infor.png",
   },
   {
     id: 1,
