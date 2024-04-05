ff56f35897323cfd7472c154cea0b926c371cf92
Anshumaan Singh Hada <69346510+AnshumaanSinghHada@users.noreply.github.com>
2024-04-05 14:51:59 +0530

    Update JobList.ts

--- JobList.ts
+++ JobList.ts
@@ -1,53 +1,71 @@
 export const JobList: Job[] = [
      {
+    id: 25,
+    heading: "Data Analyst | Wipro | Hyderabad",
+    link: "https://careers.wipro.com/careers-home/jobs/3066940",
+    image_url: "/static/img/hp.png",
+  },
+     {
+    id: 24,
+    heading: "Salesforce Developer | Pearson | Bangalore",
+    link: "https://pearson.jobs/bangalore-ind/salesforce-developer-intern/FCF1D1167E8A48CBA486F65F3F1C8296/job/?vs=1554",
+    image_url: "/static/img/hp.png",
+  },
+     {
+    id: 23,
+    heading: "Graduate Engineer Trainee | Invesco | Hyderabad",
+    link: "https://invesco.wd1.myworkdayjobs.com/en-US/intelliflo/job/Hyderabad-Telangana/Graduate-Engineer-Trainee_R-5629",
+    image_url: "/static/img/hp.png",
+  },
+     {
     id: 22,
     heading: "CXQO Associate | Amazon | Hyderabad",
     link: "https://amazonvirtualhiring.hirepro.in/registration/incta/dirc1/apply/?event=14050&job=58038",
     image_url: "/static/img/hp.png",
   },
-     {
+  {
     id: 21,
     heading: "Domain Trainee | Genpact | Jaipur",
     link: "https://genpact.taleo.net/careersection/sgy_external_career_section/jobdetail.ftl?job=INS023413",
     image_url: "/static/img/hp.png",
   },
-     {
+  {
     id: 20,
     heading: "QA Engineer | ZOHO Corp | Chennai",
     link: "https://www.zoho.com/careers/jobdetails/?job_id=2803000614913581",
     image_url: "/static/img/hp.png",
   },
-    {
+  {
     id: 19,
     heading: "Consultant Internship | Deloitte | Bangalore",
     link: "https://jobsindia.deloitte.com/job/Bengaluru-Consulting-Internship/23914944/",
     image_url: "/static/img/hp.png",
   },
-    {
+  {
     id: 18,
     heading: "Software Development Engineer | Urban Company | Gurgaon",
     link: "https://careers.urbancompany.com/jobDetail?id=eb369381-1cfe-432f-affc-12070d83a2c6",
     image_url: "/static/img/hp.png",
   },
-    {
+  {
     id: 17,
     heading: "Analyst | EY | Noida",
     link: "https://careers.ey.com/ey/job/Noida-Analyst-Forensics-National-ASU-Forensics-Investigations-&-Compliance-Noida-UP-201304/1003553001/",
     image_url: "/static/img/hp.png",
   },
-   {
+  {
     id: 16,
     heading: "Quality Analyst | Revolut | Work From Home",
     link: "https://www.revolut.com/en-IN/careers/position/quality-assurance-analyst-0f7bfdfb-6fd1-4fbe-b586-f62f84536fa9/",
     image_url: "/static/img/hp.png",
   },
-   {
+  {
     id: 15,
     heading: "Software Engineer Intern | NIKE | Bangalore",
     link: "https://jobs.nike.com/job/R-24752?cp=76060516106_aff_je6NUbpObpQ&ranMID=41134&ranEAID=je6NUbpObpQ&ranSiteID=je6NUbpObpQ-UmBg0FI_AoHwGdLO5ac13w",
     image_url: "/static/img/hp.png",
   },
-   {
+  {
     id: 14,
     heading: "iOS Developer | Meesho | Bangalore",
     link: "https://jobs.lever.co/meesho/6e0e5074-e1bb-41c6-89bb-2c20d36d4b0f/",
